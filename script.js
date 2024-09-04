e5eb9f432fc9d92d53059a08a0168d7a14c97c4e
Ledjan Miçani <ledjanmicani@gmail.com>
2024-09-04 08:13:26 -0400

    Implementation of the Done and Delete buttons

--- script.js
+++ script.js
@@ -20,11 +20,36 @@ let todos = [
 function updateTodoList() {
     list.innerHTML = '';
 
-    todos.forEach(todo => {
+    todos.forEach((todo, index) => {
         const li = document.createElement('li');
         li.textContent = todo.title;
+
+        const doneButton = document.createElement('button')
+        doneButton.textContent = 'Done';
+        doneButton.style.margin = "10px";
+        doneButton.addEventListener('click', () => {
+            todos[index].isCompleted = !todos[index].isCompleted;
+            updateTodoList();
+        })
+        
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.style.margin = '10px';
+        deleteButton.addEventListener('click', () => {
+            todos.splice(index, 1);
+            updateTodoList();
+        })
+
+        if (todo.isCompleted) {
+            li.style.textDecoration = 'line-through';
+        }
+
+        li.appendChild(doneButton);
+        li.appendChild(deleteButton);
         list.appendChild(li);
+    
     })
+
     let numOfTasks = todos.length;
     title.textContent = (numOfTasks === 0 ? `You have 0 to-dos`: `You have ${numOfTasks} to-dos`)
 }
@@ -38,7 +63,7 @@ const newItem = document.querySelector('input')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
     const newTodoTitle = newItem.value.trim();
 
     if (newTodoTitle) {
